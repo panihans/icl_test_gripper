932968bda4c24197ead5a37ba705bbf1c199e554
user <user@email>
2022-01-17 01:10:03 +0200

    timer testing

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,27 @@ void setup_adc() {
   ADC->ADC_COR = ADC_COR_DIFF4 | ADC_COR_DIFF5 | ADC_COR_OFF4 | ADC_COR_OFF5 | 
                   ADC_COR_DIFF6 | ADC_COR_DIFF7 | ADC_COR_OFF6 | ADC_COR_OFF7; // enable differential for channels and 0.5 offsets
   ADC->ADC_CHER |= ADC_CHER_CH4 | ADC_CHER_CH5 | ADC_CHER_CH6 | ADC_CHER_CH7;
+
+  ADC->ADC_IER |= ADC_IER_EOC4 | ADC_IER_EOC6; // enable interrupts
+  NVIC_EnableIRQ(ADC_IRQn);
+}
+
+void setup_timer() {
+  PMC->PMC_PCER0 |= PMC_PCER0_PID27;
+
+  TC_Configure(TC0, 0, TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_WAVSEL_UP | TC_CMR_WAVE);
+  TC_SetRC(TC0, 0, 42000);
+  TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
+  NVIC_EnableIRQ(TC0_IRQn);
 }
 
 void setup() {
   // put your setup code here, to run once:
   setup_adc();
+  setup_timer();
   Serial.begin(115200);
   Serial.println("begin");
+  Serial.println((String)"load_open,shunt_open,load_high,shunt_high");
 }
 
 #define CAN_READ_ADC_4 (ADC->ADC_ISR & ADC_ISR_EOC4)
@@ -39,40 +53,136 @@ void setup() {
 
 void get_adc_readings(uint32_t& load, uint32_t& shunt) {
   while(!CAN_READ_ADC_4) asm volatile("NOP");
-  // Serial.println(ADC->ADC_ISR, BIN);
   load = ADC->ADC_CDR[4];
   while(!CAN_READ_ADC_6) asm volatile("NOP");
-  // Serial.println(ADC->ADC_ISR, BIN);
   shunt = ADC->ADC_CDR[6];
 }
 
 #define VOUT 3.3f
 #define ADC_MAX 4095.f
 #define ADCtoV(adc) ((adc) / ADC_MAX * (VOUT * 2) - VOUT)
-#define VtoADC(v) ((v) / VOUT + 1) * (ADC_MAX / 2)
+#define VtoADC(v) ((uint32_t)(((v) / VOUT + 1) * (ADC_MAX / 2)))
 
-uint32_t adcSetpoint = VtoADC(3);
+#define ADC_SET_MIN VtoADC(-1.29);
+#define ADC_SET_MAX VtoADC(1.29);
+#define ADC_SET_ZERO VtoADC(0);
+uint32_t adcSetpoint = ADC_SET_ZERO;
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while(Serial.available()) {
+    char c = Serial.read();
+    switch(c) {
+      case 'f': adcSetpoint = ADC_SET_MAX; break;
+      case 'r': adcSetpoint = ADC_SET_MIN; break;
+      case 'b': adcSetpoint = ADC_SET_ZERO; break;
+    }
+  }
   COAST();
   TRIGGER_ADC();
-  uint32_t load = -1;
-  uint32_t shunt = -1;
-  get_adc_readings(load, shunt);
-  Serial.println((String)"c," + load + "," + shunt);
+  uint32_t load_open = -1;
+  uint32_t shunt_open = -1;
+  get_adc_readings(load_open, shunt_open);
 
-  if (load < adcSetpoint) {
+  if (adcSetpoint == VtoADC(0)) {
+    BRAKE();
+  } else if (load_open < adcSetpoint) {
     FORWARD();
-    TRIGGER_ADC();
-    load = -1;
-    shunt = -1;
-    get_adc_readings(load, shunt);
-    delay(1);
-    Serial.println((String)"f," + load + "," + shunt);
-    COAST();
+  } else if (load_open > adcSetpoint) {
+    REVERSE();
   }
+  TRIGGER_ADC();
+  uint32_t load_high = -1;
+  uint32_t shunt_high = -1;
+  get_adc_readings(load_high, shunt_high);
+  delay(1);
+  COAST();
 
-  Serial.println(".");
+  Serial.println((String)"" + load_open + "," + shunt_open + "," + load_high + "," + shunt_high);
   delay(1000);
 }
+
+void TC0_Handler() {
+  t = 1;
+  TC0->TC_CHANNEL[0].TC_SR;
+  TC_Start(TC0, 0);
+}
+
+void ADC_Handler() {
+  readADC();
+  uint32_t load = -1;
+  uint32_t shunt = -1;
+  if (CAN_READ_ADC_4) {
+    load = ADC->ADC_CDR[4];
+  }
+  if (CAN_READ_ADC_6) {
+    shunt = ADC->ADC_CDR[6];
+  }
+}
+
+
+// /*
+//  * rosserial Publisher Example
+//  * Prints "hello world!"
+//  */
+
+// #include <ros.h>
+// #include <std_msgs/String.h>
+
+// class NewHardware : public ArduinoHardware
+// {
+//   public:
+//   NewHardware():ArduinoHardware(&Serial1, 57600){};
+// };
+
+// ros::NodeHandle_<NewHardware>  nh;
+
+
+// std_msgs::String str_msg;
+// ros::Publisher chatter("chatter", &str_msg);
+
+// char hello[] = "1234";
+
+// void setup()
+// {
+//   nh.initNode();
+//   nh.advertise(chatter);
+// }
+
+// void loop()
+// {
+//   str_msg.data = hello;
+//   chatter.publish( &str_msg );
+//   nh.spinOnce();
+//   delay(1);
+// }
+
+// volatile uint8_t t = 0;
+// void TC0_Handler() {
+//   t = 1;
+//   TC0->TC_CHANNEL[0].TC_SR;
+//   TC_Start(TC0, 0);
+// }
+
+// void setup_timer() {
+//   PMC->PMC_PCER0 |= PMC_PCER0_PID27;
+
+//   TC_Configure(TC0, 0, TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_WAVSEL_UP | TC_CMR_WAVE);
+//   TC_SetRC(TC0, 0, 42000);
+//   TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
+//   NVIC_EnableIRQ(TC0_IRQn);
+// }
+
+// void setup() {
+//   Serial.begin(115200);
+// }
+
+// void loop() {
+//   if (t) {
+//     t = 0;
+//     Serial.println((String)"timer: " + millis());
+//   }
+//   TC_Start(TC0, 0);
+//   // Serial.println((String)"loop: " + millis());
+//   // delay(1000);
+// }
